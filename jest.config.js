519ef7738f7f4c0233d3d328cb58cba5bbd518ae
Johan Gustafsson <johan.gustafsson91@gmail.com>
2022-11-19 15:37:00 +0100

    build: bump multiple deps

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,10 @@
-module.exports = {
+// jest.config.js
+const nextJest = require("next/jest");
+
+// Providing the path to your Next.js app which will enable loading next.config.js and .env files
+const createJestConfig = nextJest({ dir: "./" });
+
+const customJestConfig = {
   roots: ["<rootDir>"],
   modulePaths: ["src"],
   testEnvironment: "./jsdom-extended",
@@ -6,10 +12,10 @@ module.exports = {
   moduleFileExtensions: ["ts", "tsx", "js", "json", "jsx"],
   testPathIgnorePatterns: ["<rootDir>[/\\\\](node_modules|.next)[/\\\\]"],
   transformIgnorePatterns: ["[/\\\\]node_modules[/\\\\].+\\.(ts|tsx)$"],
-  transform: {
-    "^.+\\.(ts|tsx)$": "babel-jest",
-  },
   verbose: true,
   collectCoverage: true,
   coverageReporters: ["json", "lcov", "text", "text-summary"],
 };
+
+// createJestConfig is exported in this way to ensure that next/jest can load the Next.js configuration, which is async
+module.exports = createJestConfig(customJestConfig);
